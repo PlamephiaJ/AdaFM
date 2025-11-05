90ca933d485d9b951cb6ecd12bbf193c71a54793
YHC <yu.hao.c@outlook.com>
2025-11-04 17:23:17 -0800

    add adafm grid search

--- scripts/runcav.sh
+++ scripts/runcav.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=0
 python main.py -m \
     optimizers=pesg \
     optimizers.lr_x=0.1,0.001,0.05 \
-    optimizers.lr_y=0.1,0.001,0.05 \
+    optimizers.lr_y=0.1,0.001,0.05
