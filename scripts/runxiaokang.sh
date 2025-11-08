7fa20c8513a30cb6937d37805132d9e81c63e562
YHC <moyunyongan@gmail.com>
2025-11-07 16:02:47 -0800

    add xk exp

--- scripts/runxiaokang.sh
+++ scripts/runxiaokang.sh
@@ -1,4 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
 python main.py -m \
-    optimizers=adafm,pesg,msgda,tiada \
-    models/backbone=wgan-gp-light
+    optimizers=adagrad \
+    models/backbone=wgan-gp-in
