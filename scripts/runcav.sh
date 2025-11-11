502054c47c58015692ac93c878d8085a3abc539a
YHC <moyunyongan@gmail.com>
2025-11-10 19:03:50 -0800

    rerun cifar10

--- scripts/runcav.sh
+++ scripts/runcav.sh
@@ -1,5 +1,5 @@
 export CUDA_VISIBLE_DEVICES=0
 python main.py -m \
-    datasets=cifar10,cifar100 \
+    datasets=cifar10 \
     optimizers=adafm \
-    optimizers.delta=1e-3,0.1,0.2
+    optimizers.delta=0,1e-3,0.1,0.2
