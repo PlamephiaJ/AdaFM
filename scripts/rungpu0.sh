6a9cd3e381190c6f99643b4c94287de6e5db5330
YHC <yu.hao.c@outlook.com>
2025-11-13 21:08:16 -0800

    add gs

--- scripts/rungpu0.sh
+++ scripts/rungpu0.sh
@@ -1,2 +1,4 @@
 export CUDA_VISIBLE_DEVICES=0
-python main.py
+python main.py - m \
+    lr_x=0.0001,0.001,0.01,0.1,1 \
+    lr_y=0.0001,0.001,0.01,0.1,1 \
--- scripts/rungpu1.sh
+++ scripts/rungpu1.sh
@@ -1,3 +1,4 @@
 export CUDA_VISIBLE_DEVICES=1
-python main.py \
-    optimizers=adafm
+python main.py - m \
+    lr_x=0.0005,0.005,0.05,0.5,0.07 \
+    lr_y=0.0005,0.005,0.05,0.5,0.07 \
