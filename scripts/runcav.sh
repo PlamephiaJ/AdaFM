f633ef6af3ba9dedde335de7f09085000f80e0ac
YHC <moyunyongan@gmail.com>
2025-11-14 17:21:56 -0800

    add exp

--- scripts/runcav.sh
+++ scripts/runcav.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 export CUDA_VISIBLE_DEVICES=0
-python main.py -m optimizers=pesg \
-    optimizers.lr=0.0001 \
-    models.evaluation.use_fid=false
-    #,0.001,0.01,0.1,1 \
+python main.py -m optimizers=msgda \
+    optimizers.lr_discriminator=0.0001,0.001,0.01,0.1,1 \
+    optimizers.lr_generator=0.0001,0.001,0.01,0.1,1
