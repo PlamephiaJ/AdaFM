7dcdbbff0515388ddcf3110eded3d6c97aaffe39
YHC <yu.hao.c@outlook.com>
2025-11-18 17:11:07 -0800

    add best config

--- scripts/rungpu0.sh
+++ scripts/rungpu0.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES=0
-python main.py -m optimizers=rmsprop \
-     optimizers.lr_x=0.0001,0.001,0.01,0.1,1 \
-     optimizers.lr_y=0.0001,0.001,0.01,0.1,1
+python main.py -m optimizers=rmsprop,tiada
--- scripts/rungpu1.sh
+++ scripts/rungpu1.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES=1
-python main.py -m optimizers=adafm \
-     optimizers.lr_x=0.0001,0.001,0.01,0.1,1 \
-     optimizers.lr_y=0.0001,0.001,0.01,0.1,1
+python main.py -m optimizers=adam,adafm
