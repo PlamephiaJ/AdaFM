438c361a0591f1f4fa4bd5f5b0cf303919ab571e
YHC <yu.hao.c@outlook.com>
2025-11-19 17:25:49 -0800

    add cav msgda

--- scripts/runcav.sh
+++ scripts/runcav.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 export CUDA_VISIBLE_DEVICES=0
-python main.py -m optimizers=msgda \
-    optimizers.lr_discriminator=0.0001,0.001,0.01,0.1,1 \
-    optimizers.lr_generator=0.0001,0.001,0.01,0.1,1
+python main.py -m optimizers=msgda
